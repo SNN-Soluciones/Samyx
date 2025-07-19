e471a8a3e0189edead3fa2df63dadc8aa18ab9a3
Andres Mayorga Espinoza <andres.mayorgaespinoza@nordstrom.com>
2025-07-19 00:06:52 -0600

    Mas cambios en los thymelafs

--- src/main/java/snn/soluciones/com/models/dao/ICTerminalUsuarioDao.java
+++ src/main/java/snn/soluciones/com/models/dao/ICTerminalUsuarioDao.java
@@ -9,17 +9,17 @@ import snn.soluciones.com.models.entity.CTerminalUsuario;
 public interface ICTerminalUsuarioDao extends CrudRepository<CTerminalUsuario, Long> {
   @Query("SELECT c FROM CTerminalUsuario c JOIN FETCH c.terminal t WHERE t.sucursal.id = ?1")
   List<CTerminalUsuario> findAllBySucursal(Long paramLong);
-  
+
   @Modifying
   @Query("DELETE FROM CTerminalUsuario c WHERE c.id = ?1")
   void deleteAccesoUsuarioById(Long paramLong);
-  
-  @Query("SELECT c FROM CTerminalUsuario c JOIN FETCH c.terminal t JOIN FETCH t.sucursal s WHERE s.emisor.id=?1 AND c.usuario.id=?2 GROUP BY s.id")
+
+  @Query("SELECT DISTINCT c FROM CTerminalUsuario c JOIN FETCH c.terminal t JOIN FETCH t.sucursal s WHERE s.emisor.id=?1 AND c.usuario.id=?2")
   List<CTerminalUsuario> findAllByEmisorAndSucursal(Long paramLong1, Long paramLong2);
-  
+
   @Query("SELECT c FROM CTerminalUsuario c JOIN FETCH c.terminal t JOIN FETCH t.sucursal s WHERE s.id=?1 AND c.usuario.id=?2")
   List<CTerminalUsuario> findAllBySucursalByUsuario(Long paramLong1, Long paramLong2);
-  
+
   @Query("SELECT c FROM CTerminalUsuario c JOIN FETCH c.terminal t JOIN FETCH t.sucursal s WHERE s.id=?1 AND t.id = ?2 AND c.usuario.id = ?3")
   CTerminalUsuario findSucursalTerminalBySucursalByTerminal(Long paramLong1, Long paramLong2, Long paramLong3);
 }
