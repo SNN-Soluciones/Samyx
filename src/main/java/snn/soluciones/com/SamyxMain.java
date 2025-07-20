66b973e962c690dd884544a2d218bfff8b67ee0a
Andres Mayorga Espinoza <andres.mayorgaespinoza@nordstrom.com>
2025-07-19 19:44:33 -0600

    Eureka

--- src/main/java/snn/soluciones/com/SamyxMain.java
+++ src/main/java/snn/soluciones/com/SamyxMain.java
@@ -4,8 +4,10 @@ import java.util.TimeZone;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SamyxMain {
   @PostConstruct
   public void init() {
